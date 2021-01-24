0495d2021be55eacdc4e6d22bb9105ee7dc99135
Guillaume Huard Hughes <guillaumehuardhughes@gmail.com>
2021-01-24 08:25:53 -0500

    Add events listener with socket

--- README.md
+++ README.md
@@ -6,6 +6,13 @@
 - You can ask for some feature in the issue section with the enhancement label.
 - If you want contribute to the project, go for it!
 
+## Install
+
+- Make sure you know how to use [React.js](https://reactjs.org/)
+- `npm install` to install packages
+- `npm run start` to run the project locally
+- `npm run build` to build project and get final files
+
 ## How to import to streamElements
 
 - HTML: Copy `build/static/index.html` for the HTML section
@@ -13,12 +20,35 @@
 - CSS: Copy `build/static/css/bundle.min.css` for the css section
 - Image: Don't worry buddy, images are inline into base64. (Nothing to do)
 
+## Listen EVENTS (follow, subs, etc)
+
+I already integrated that part: <https://docs.streamelements.com/docs/connecting-via-websocket>.
+So you just have to create your .env file and put your token on `REACT_APP_STREAMELEMENTS_TOKEN`.
+
+### env
+
+Create the `.env` file at the root of the project and add your token to the `REACT_APP_STREAMELEMENTS_TOKEN` env variable.
+
+- You can get you token from: https://streamelements.com/dashboard/account/channels
+- Show secrets
+- copy you JWT token
+- paste it in your `.env` file
+
+Example:
+
+```.env
+REACT_APP_STREAMELEMENTS_TOKEN=my_stream_element_token
+```
+
 ## Nice packages integrated
 
 - Make simple and nice animation with: [react-animated-css](https://github.com/digital-flowers/react-animated-css#readme)
 
+## StreamElements API Documentation (Events)
+
+https://github.com/StreamElements/widgets/blob/master/CustomCode.md
+
 ## TODO
 
-- Simulate data
-- Simulate onWidgetLoad
-- Simulate onEventReceived (API)
+- Simulate onWidgetLoad/data
+- Clean and restructure code (Socket/events)
--- src/App.js
+++ src/App.js
@@ -3,14 +3,81 @@ import { Animated } from "react-animated-css";
 import "./App.css";
 
 import logo from "./logo.svg";
+// import io from "socket.io-client";
 
 function App() {
   const [isLogoVisible, setIsLogoVisible] = useState(true);
+  const [latestFollower, setLatestFollower] = useState();
+
+  const JWT = process.env.REACT_APP_STREAMELEMENTS_TOKEN;
+
+  // eslint-disable-next-line no-undef
+  const socket = io("https://realtime.streamelements.com", {
+    transports: ["websocket"],
+  });
+
+  const onConnect = () => {
+    console.log("Successfully connected to the websocket");
+    socket.emit("authenticate", {
+      method: "jwt",
+      token: JWT,
+    });
+  };
+
+  const onDisconnect = () => {
+    console.log("Disconnected from websocket");
+    // Reconnect
+  };
+
+  const onAuthenticated = (data) => {
+    const { channelId } = data;
+
+    console.log(`Successfully connected to channel ${channelId}`);
+  };
+
+  const onEventUpdateTest = (data) => {
+    console.log("onEventUpdateTest:", data);
+
+    const followerName = data.event.name;
+    if (data.listener !== "follower-latest" || latestFollower === followerName)
+      return;
+
+    setLatestFollower(data.event.name);
+    console.log("A new FOLLOWER!!!", followerName);
+  };
 
   useEffect(() => {
     setTimeout(() => {
       setIsLogoVisible(false);
     }, 5000);
+  }, []);
+
+  useEffect(() => {
+    // Socket connected
+    socket.on("connect", onConnect);
+
+    // Socket got disconnected
+    socket.on("disconnect", onDisconnect);
+
+    // Socket is authenticated
+    socket.on("authenticated", onAuthenticated);
+
+    socket.on("event:test", onEventUpdateTest);
+
+    socket.on("event", (data) => {
+      console.log(data);
+      // Structure as on JSON Schema
+    });
+
+    socket.on("event:update", (data) => {
+      console.log(data);
+      // Structure as on https://github.com/StreamElements/widgets/blob/master/CustomCode.md#on-session-update
+    });
+
+    socket.on("event:reset", (data) => {
+      console.log(data);
+      // Structure as on https://github.com/StreamElements/widgets/blob/master/CustomCode.md#on-session-update
+    });
   }, []);
 
   return (
